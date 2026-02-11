3129b0df29612734fbe36a583bad066f1f57b41f
Marc-Antoine Berger <marberge@z1r9p1.42lyon.fr>
2026-02-11 17:04:51 +0100

    ex1

---
+++ 01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    name: str = "Rose"
+    height: int = 25
+    age: int = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}")
+    print(f"Age: {age}\n")
+    print("=== End of Program ===")
