5d02229748cb26a8328f13041a1381fa4cfc1202
Marc-Antoine Berger <marberge@z1r10p2.42lyon.fr>
2026-03-02 23:16:17 +0100

    ex2

--- 03/ex2/ft_coordinate_system.py
+++ 03/ex2/ft_coordinate_system.py
@@ -3,7 +3,7 @@ import math
 
 
 def calc_distance(p1: tuple[int, int, int], p2: tuple[int, int, int]) -> float:
-    """Calculates the distance between two 3D points."""
+    """Calculates and returns the distance between two 3D points."""
     x1, y1, z1 = p1
     x2, y2, z2 = p2
 
@@ -11,7 +11,7 @@ def calc_distance(p1: tuple[int, int, int], p2: tuple[int, int, int]) -> float:
     return distance
 
 
-def parse_coordinate_str(coord_str: str) -> tuple[int, int, int] | None:
+def parse_coord_str(coord_str: str) -> tuple[int, int, int] | None:
     """Parses a string like 'x,y,z' into a tuple of integers."""
     try:
         parts = coord_str.split(',')
@@ -30,29 +30,32 @@ def parse_coordinate_str(coord_str: str) -> tuple[int, int, int] | None:
 
 def main() -> None:
     """Demonstrate the calul between two 3D points"""
-    print("=== Game Coordinate System ===\n")
 
     origin: tuple[int, int, int] = (0, 0, 0)
+    pos1: tuple[int, int, int] | None = None
 
     try:
         if len(sys.argv) > 1:
             if len(sys.argv) > 2:
                 raise ValueError("Error : too much arguments !")
             else:
-                str = sys.argv[1]
-                pos1: tuple[int, int, int] = parse_coordinate_str(str)
+                coord_str: str = sys.argv[1]
+                pos1 = parse_coord_str(coord_str)
+                if pos1 is None:
+                    return
         else:
             pos1 = (10, 20, 5)
     except Exception as e:
         print(e)
         return
+    print("=== Game Coordinate System ===\n")
     print(f"Position created: {pos1}")
     dist1 = calc_distance(origin, pos1)
     print(f"Distance between {origin} and {pos1}: {round(dist1, 2)}\n")
 
     coord_str_valid = "3,4,0"
     print(f'Parsing coordinates: "{coord_str_valid}"')
-    pos2 = parse_coordinate_str(coord_str_valid)
+    pos2 = parse_coord_str(coord_str_valid)
 
     if pos2 is not None:
         print(f"Parsed position: {pos2}")
@@ -61,7 +64,7 @@ def main() -> None:
 
     coord_str_invalid = "abc,def,ghi"
     print(f'Parsing invalid coordinates: "{coord_str_invalid}"')
-    parse_coordinate_str(coord_str_invalid)
+    parse_coord_str(coord_str_invalid)
     print()
 
     print("Unpacking demonstration:")
